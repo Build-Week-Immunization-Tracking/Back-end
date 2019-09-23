6b11737de4e2a2dd81cbd79481ddd6e2e0d37d29
Richard Lawson <lawson.a.richard@gmail.com>
2019-09-23 14:08:38 -0700

    created paitents.js helper
    created paitent.js helper

---
+++ data/helpers/patients.js
@@ -0,0 +1,66 @@
+const db = require("../dbConfig");
+
+module.exports = {
+  getPatients,
+  getPatientbyId,
+  addPatient,
+  updatePatient,
+  deletePatient,
+  getPermittedProviders,
+  getPatientsForProvider
+};
+
+function getPatients(userId) {
+  return db("patients").where({ userId });
+}
+
+function getPatientbyId(id) {
+  return db("patients").where({ id });
+}
+
+function addPatient(patient) {
+  return db("patients")
+    .returning("id")
+    .insert(patient);
+}
+
+function updatePatient(id, update) {
+  return db("patients")
+    .where({ id })
+    .returning([
+      "id",
+      "firstName",
+      "lastName",
+      "birthDate",
+      "userId",
+      "createdAt"
+    ])
+    .update(update);
+}
+
+function deletePatient(id) {
+  return db("patients")
+    .where({ id })
+    .del();
+}
+
+function getPermittedProviders(id) {
+  return db("patients")
+    .where({ "patients.id": id })
+    .join("permissions", { "patients.id": "permissions.patientId" })
+    .join("providers", { "permissions.providerId": "providers.id" })
+    .select("providers.id", "providers.name");
+}
+
+function getPatientsForProvider(providerId) {
+  return db("permissions")
+    .where({ providerId })
+    .join("patients", { "patients.id": "permissions.patientid" })
+    .select(
+      "patients.id",
+      "patients.firstName",
+      "patients.lastName",
+      "patients.birthDate",
+      "patients.createdAt"
+    );
+}
