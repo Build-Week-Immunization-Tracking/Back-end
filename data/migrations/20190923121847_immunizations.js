32a85917691c46fc4176d90256e80511afe808a9
Richard Lawson <lawson.a.richard@gmail.com>
2019-09-23 13:39:51 -0700

    created immuniztions
    seeds and migration for immuniztions created and functioning

--- data/migrations/20190923121847_immunizations.js
+++ data/migrations/20190923121847_immunizations.js
@@ -1,8 +1,14 @@
-
 exports.up = function(knex) {
-  
+  return knex.schema.createTable("immunizations", tbl => {
+    tbl.increments();
+    tbl
+      .string("name", 255)
+      .notNullable()
+      .unique();
+    tbl.datetime("createAt").defaultTo(knex.fn.now());
+  });
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists("immunizations");
 };
