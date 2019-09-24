2f6d5fdba2523d78c5b60e088f03d96930971295
Richard Lawson <lawson.a.richard@gmail.com>
2019-09-24 13:59:45 -0700

    Added Seeds
    added seeds to immunization

--- data/seeds/dev/03-immunizations.js
+++ data/seeds/dev/03-immunizations.js
@@ -5,7 +5,21 @@ exports.seed = function(knex) {
     .then(function() {
       // Inserts seed entries
       return knex("immunizations").insert([
-        { name: "Hepatitis B Vaccine - (HepB)" }
+        { name: "Hepatitis B Vaccine - (HepB)" },
+        { name: "Rotavirus - (RV) RV1 (2-dose series); RV5 (3-dose series)" },
+        { name: "Diphtheria, tetanus, & acellular pertussis - DTap" },
+        { name: "Haemophilus influenzae type b - (Hib)" },
+        { name: "Pneumococcal conjugate - (PCV13)" },
+        { name: "Inactivated poliovirus - (IPV)" },
+        { name: "Influenza - (IIV)" },
+        { name: "Influenza - (LAIV)" },
+        { name: "Measles, mumps, rubella - (MMR)" },
+        { name: "Varicella - (VAR)" },
+        { name: "Meningococcal - (MenACWY-D)" },
+        { name: "Tetanus, diphtheria, & acellular pertussis - (Tdap)" },
+        { name: "Human papillomavirus - (HPV)" },
+        { name: "Meningococcal B - (MenB)" },
+        { name: "Pneumococcal polysaccharide - (PPSV23)" }
       ]);
     });
 };
